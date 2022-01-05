202a8314f382e52843117b3b04bd61db0dd8f2d8
Ingrid <intarga@protonmail.com>
2022-01-05 17:13:29 +0100

    implement wrapping elements

--- lua/embrace/embrace.lua
+++ lua/embrace/embrace.lua
@@ -4,6 +4,14 @@ local function nearest_matching_brace(flags)
     return unpack(vim.api.nvim_call_function('searchpairpos', {'(', '', ')', flags}))
 end
 
+local function find_elem_end(flags)
+    return unpack(vim.api.nvim_call_function('searchpos', {[[)\|\ ]], 'W'}))
+end
+
+local function find_elem_start(flags)
+    return unpack(vim.api.nvim_call_function('searchpos', {[[(\|\ ]], 'bW'}))
+end
+
 local function char_under_cursor()
     local col = vim.api.nvim_win_get_cursor(0)[2]
     return vim.api.nvim_get_current_line():sub(col+1,col+1)
@@ -37,6 +45,24 @@ function M.insert_after_list()
     vim.api.nvim_put({'('}, 'c', false, false)
 
     nearest_matching_brace('W')
+    vim.api.nvim_put({' )'}, 'c', false, false)
+
+    vim.api.nvim_command('startinsert')
+end
+
+function M.insert_before_elem()
+    find_elem_end()
+    vim.api.nvim_put({')'}, 'c', false, false)
+    find_elem_start()
+    vim.api.nvim_put({'( '}, 'c', true, false)
+
+    vim.api.nvim_command('startinsert')
+end
+
+function M.insert_after_elem()
+    find_elem_start()
+    vim.api.nvim_put({'('}, 'c', true, false)
+    find_elem_end()
     vim.api.nvim_put({' )'}, 'c', false, false)
 
     vim.api.nvim_command('startinsert')
--- plugin/embrace.vim
+++ plugin/embrace.vim
@@ -7,6 +7,8 @@ command! EmbraceNext       lua require("embrace").move_next_matching_brace()
 command! EmbracePrev       lua require("embrace").move_prev_matching_brace()
 command! EmbraceInsertList lua require("embrace").insert_before_list()
 command! EmbraceAppendList lua require("embrace").insert_after_list()
+command! EmbraceInsertElem lua require("embrace").insert_before_elem()
+command! EmbraceAppendElem lua require("embrace").insert_after_elem()
 
 let &cpo = s:save_cpo " and restore after
 unlet s:save_cpo
