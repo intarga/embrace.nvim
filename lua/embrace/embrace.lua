fb8d53040309d0a29ee786396e6263c717e0f948
Ingrid <intarga@protonmail.com>
2022-01-05 17:06:39 +0100

    implement wrapping lists

--- lua/embrace/embrace.lua
+++ lua/embrace/embrace.lua
@@ -4,12 +4,42 @@ local function nearest_matching_brace(flags)
     return unpack(vim.api.nvim_call_function('searchpairpos', {'(', '', ')', flags}))
 end
 
+local function char_under_cursor()
+    local col = vim.api.nvim_win_get_cursor(0)[2]
+    return vim.api.nvim_get_current_line():sub(col+1,col+1)
+end
+
 function M.move_next_matching_brace()
     nearest_matching_brace('W')
 end
 
 function M.move_prev_matching_brace()
     nearest_matching_brace('bW')
+end
+
+function M.insert_before_list()
+    if char_under_cursor() ~= ')' then
+        nearest_matching_brace('W')
+    end
+    vim.api.nvim_put({')'}, 'c', false, false)
+
+    nearest_matching_brace('bW')
+    vim.api.nvim_put({'( '}, 'c', false, false)
+
+    vim.api.nvim_command('startinsert')
+end
+
+function M.insert_after_list()
+    if char_under_cursor() ~= '(' then
+        nearest_matching_brace('bW')
+    end
+
+    vim.api.nvim_put({'('}, 'c', false, false)
+
+    nearest_matching_brace('W')
+    vim.api.nvim_put({' )'}, 'c', false, false)
+
+    vim.api.nvim_command('startinsert')
 end
 
 return M
--- plugin/embrace.vim
+++ plugin/embrace.vim
@@ -3,8 +3,10 @@ if exists('g:loaded_embrace') | finish | endif " prevent loading file twice
 let s:save_cpo = &cpo " save user coptions
 set cpo&vim           " reset them to defaults
 
-command! EmbraceNext lua require("embrace").move_next_matching_brace()
-command! EmbracePrev lua require("embrace").move_prev_matching_brace()
+command! EmbraceNext       lua require("embrace").move_next_matching_brace()
+command! EmbracePrev       lua require("embrace").move_prev_matching_brace()
+command! EmbraceInsertList lua require("embrace").insert_before_list()
+command! EmbraceAppendList lua require("embrace").insert_after_list()
 
 let &cpo = s:save_cpo " and restore after
 unlet s:save_cpo
